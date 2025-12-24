b2054269e2cbbe04f45aec9929c2b002813e4ec2
Marazm <nikitazolotovvv@gmail.com>
2025-12-25 02:31:14 +0300

    Реализованы функции для homework01

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,28 +1,27 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
-    """
-    Encrypts plaintext using a Vigenere cipher.
-    >>> encrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> encrypt_vigenere("python", "a")
-    'python'
-    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
-    'LXFOPVEFRNHR'
-    """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    key_len = len(keyword)
+    for i, char in enumerate(plaintext):
+        k = keyword[i % key_len].lower()
+        shift = ord(k) - ord('a')
+        if char.isupper():
+            ciphertext += chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
+        elif char.islower():
+            ciphertext += chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+        else:
+            ciphertext += char
     return ciphertext
 
-
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
-    """
-    Decrypts a ciphertext using a Vigenere cipher.
-    >>> decrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> decrypt_vigenere("python", "a")
-    'python'
-    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
-    'ATTACKATDAWN'
-    """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    key_len = len(keyword)
+    for i, char in enumerate(ciphertext):
+        k = keyword[i % key_len].lower()
+        shift = ord(k) - ord('a')
+        if char.isupper():
+            plaintext += chr((ord(char) - ord('A') - shift) % 26 + ord('A'))
+        elif char.islower():
+            plaintext += chr((ord(char) - ord('a') - shift) % 26 + ord('a'))
+        else:
+            plaintext += char
     return plaintext
