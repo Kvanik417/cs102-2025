f820f1ba3aeaa3c707f1782ae6ab3da59a316ed5
Никита Золотов <94174194+Kvanik417@users.noreply.github.com>
2025-12-25 05:23:09 +0300

    Update rsa.py

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -7,6 +7,7 @@ def gcd(a: int, b: int) -> int:
         a, b = b, a % b
     return a
 
+
 def is_prime(n: int) -> bool:
     if n < 2:
         return False
@@ -15,15 +16,15 @@ def is_prime(n: int) -> bool:
             return False
     return True
 
+
 def multiplicative_inverse(e: int, phi: int) -> int:
     original_phi = phi
     x0, x1 = 0, 1
     while e > 0:
-        q, phi, e = phi // e, e, phi % e
+        q, e, phi = phi // e, phi % e, e
         x0, x1 = x1 - q * x0, x0
-    if phi != 1:
-        return 0
     return x1 % original_phi
+
 
 def generate_keypair(p: int, q: int) -> Tuple[Tuple[int, int], Tuple[int, int]]:
     n = p * q
@@ -34,12 +35,12 @@ def generate_keypair(p: int, q: int) -> Tuple[Tuple[int, int], Tuple[int, int]]:
     d = multiplicative_inverse(e, phi)
     return (e, n), (d, n)
 
-def encrypt(pk: Tuple[int, int], plaintext: str) -> int:
-    key, n = pk
-    cipher = [pow(ord(char), key, n) for char in plaintext]
-    return cipher
 
-def decrypt(pk: Tuple[int, int], ciphertext: list) -> str:
+def encrypt(pk: Tuple[int, int], plaintext: str) -> list[int]:
     key, n = pk
-    plain = [chr(pow(char, key, n)) for char in ciphertext]
-    return ''.join(plain)
+    return [pow(ord(char), key, n) for char in plaintext]
+
+
+def decrypt(pk: Tuple[int, int], ciphertext: list[int]) -> str:
+    key, n = pk
+    return "".join(chr(pow(char, key, n)) for char in ciphertext)
