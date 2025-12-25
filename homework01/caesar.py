47277a2de9cc9a6c25be8f00de54008a9eb4ed89
Никита Золотов <94174194+Kvanik417@users.noreply.github.com>
2025-12-25 05:01:46 +0300

    Update caesar.py

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -2,9 +2,9 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ciphertext = ""
     for char in plaintext:
         if char.isupper():
-            ciphertext += chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
+            ciphertext += chr((ord(char) - ord("A") + shift) % 26 + ord("A"))
         elif char.islower():
-            ciphertext += chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+            ciphertext += chr((ord(char) - ord("a") + shift) % 26 + ord("a"))
         else:
             ciphertext += char
     return ciphertext
@@ -14,9 +14,9 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     plaintext = ""
     for char in ciphertext:
         if char.isupper():
-            plaintext += chr((ord(char) - ord('A') - shift) % 26 + ord('A'))
+            plaintext += chr((ord(char) - ord("A") - shift) % 26 + ord("A"))
         elif char.islower():
-            plaintext += chr((ord(char) - ord('a') - shift) % 26 + ord('a'))
+            plaintext += chr((ord(char) - ord("a") - shift) % 26 + ord("a"))
         else:
             plaintext += char
     return plaintext
