09ce5462eb8a15c44ec0a5ead6491aef89972fac
Никита Золотов <94174194+Kvanik417@users.noreply.github.com>
2025-12-25 05:12:21 +0300

    Update rsa4.py

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -1,6 +1,10 @@
 import random
 from typing import Tuple
 
+def gcd(a: int, b: int) -> int:
+    while b != 0:
+        a, b = b, a % b
+    return a
 
 def is_prime(n: int) -> bool:
     if n < 2:
@@ -10,40 +14,31 @@ def is_prime(n: int) -> bool:
             return False
     return True
 
-
-def gcd(a: int, b: int) -> int:
-    while b != 0:
-        a, b = b, a % b
-    return a
-
-
 def multiplicative_inverse(e: int, phi: int) -> int:
-    old_r, r = phi, e
-    old_s, s = 1, 0
-    old_t, t = 0, 1
-    while r != 0:
-        quotient = old_r // r
-        old_r, r = r, old_r - quotient * r
-        old_s, s = s, old_s - quotient * s
-        old_t, t = t, old_t - quotient * t
-    return t % phi
-
+    original_phi = phi
+    x0, x1 = 0, 1
+    while e > 0:
+        q, phi, e = phi // e, e, phi % e
+        x0, x1 = x1 - q * x0, x0
+    if phi != 1:
+        return 0
+    return x1 % original_phi
 
 def generate_keypair(p: int, q: int) -> Tuple[Tuple[int, int], Tuple[int, int]]:
-    if not (is_prime(p) and is_prime(q)):
-        raise ValueError("Both numbers must be prime.")
-    elif p == q:
-        raise ValueError("p and q cannot be equal")
-
     n = p * q
     phi = (p - 1) * (q - 1)
-
-    e = random.randrange(2, phi)
-    g = gcd(e, phi)
-    while g != 1:
-        e = random.randrange(2, phi)
-        g = gcd(e, phi)
-
+    e = 3
+    while gcd(e, phi) != 1:
+        e += 2
     d = multiplicative_inverse(e, phi)
+    return (e, n), (d, n)
 
-    return ((e, n), (d, n))
+def encrypt(pk: Tuple[int, int], plaintext: str) -> int:
+    key, n = pk
+    cipher = [pow(ord(char), key, n) for char in plaintext]
+    return cipher
+
+def decrypt(pk: Tuple[int, int], ciphertext: list) -> str:
+    key, n = pk
+    plain = [chr(pow(char, key, n)) for char in ciphertext]
+    return ''.join(plain)
