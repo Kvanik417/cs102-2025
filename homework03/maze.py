280dca282efbde60f617d71c3a240eb183940497
Никита Золотов <94174194+Kvanik417@users.noreply.github.com>
2026-01-14 01:18:44 +0300

    Update maze.py

--- homework03/maze.py
+++ homework03/maze.py
@@ -64,8 +64,8 @@ def make_step(grid: List[List[Cell]], k: int) -> List[List[Cell]]:
     for x in range(len(grid)):
         for y in range(len(grid[0])):
             if grid[x][y] == k:
-                for dx, dy in [(-1,0),(1,0),(0,-1),(0,1)]:
-                    nx, ny = x+dx, y+dy
+                for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                    nx, ny = x + dx, y + dy
                     if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]) and grid[nx][ny] == 0:
                         new_coords.append((nx, ny))
     for nx, ny in new_coords:
